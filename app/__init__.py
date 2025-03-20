b5dbd17d0635b162513b436a0cde8d07f401dd34
Abhinav <abhinavpandey56393@gmail.com>
2025-03-20 13:23:37 +0530

    updated

--- app/__init__.py
+++ app/__init__.py
@@ -4,23 +4,30 @@ from app.config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_mail import Mail
 from itsdangerous import URLSafeTimedSerializer
+from flask_migrate import Migrate
 
 
 db = SQLAlchemy()
 bcrypt = Bcrypt()
 mail = Mail()
+migrate = Migrate()
 # Initialize Flask App
 def create_app():
     app = Flask(__name__, template_folder="templates")
-    app.config.from_object(Config)
-    db.init_app(app)
+    app.config.from_object(Config)  # Load configuration with previous settings restored
+
+    db.init_app(app)  # Initialize the database with the app
     bcrypt.init_app(app)
     mail.init_app(app)
+    migrate.init_app(app, db) 
     with app.app_context():
-        from app import routes, models 
         from app.routes import register_routes
-        db.create_all()
-        register_routes(app)
+    
+    db.create_all()  # Ensure all tables are created if they don't exist
+
+
+
+    register_routes(app)
     return app
 
 # Function to generate tokens
